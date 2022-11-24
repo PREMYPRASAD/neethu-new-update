7be3126ebf24732f2c7ba85ed4be1a56c2659c83
PREMYPRASAD <mailtopremy2010@gmail.com>
2022-11-24 16:44:00 +0530

    changes

---
+++ frontend/src/app/addbook/addbook.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BookserviceService } from '../bookservice.service';
+
+@Component({
+  selector: 'app-addbook',
+  templateUrl: './addbook.component.html',
+  styleUrls: ['./addbook.component.css']
+})
+export class AddbookComponent implements OnInit {
+
+  bookItem= {
+    bookName: '',
+    bookAuthorname: '',
+    bookdescription: '',
+    //bookPrice : Number,
+    //bookRate : Number,
+    bookImageurl: ''}
+  constructor(private bookService:BookserviceService,private router:Router) { }
+  
+  ngOnInit(): void {
+  }
+  AddBook()
+  {    
+    this.bookService.newBook(this.bookItem);
+    console.log("Called");    
+    alert("New Book Added Successfully");
+    this.router.navigate(['books']);
+  }
+
+}
---
+++ frontend/src/app/globals.ts
@@ -0,0 +1,2 @@
+//export const server_address: string = '/api';
+ export const server_address: string = 'http://localhost:3006/api';
